dad0b68e35b5ecdd2dda81f95f74321d7028c3f2
Haidara Mohamady Icné <ahmed.haidara.7628@gmail.com>
2023-09-22 13:37:52 +0100

    ajout des dossiers

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\etudiant_controler;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
+Route::resource('etudiants', etudiant_controler::class);
